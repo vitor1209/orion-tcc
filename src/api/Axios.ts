f80f6058e81d763fa75e3764f3427eba14181b1a
vitor1209 <vitor.lopess1711@gmail.com>
2026-06-24 10:48:50 -0300

    fix(axios): altera url para versao publicada

--- src/api/Axios.ts
+++ src/api/Axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "orionapi-production.up.railway.app",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
