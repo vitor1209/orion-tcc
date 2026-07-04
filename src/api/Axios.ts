7d130ad25b940971e6ab76b7ba299bb9143bc515
vitor1209 <vlopes@tecnologiaunica.com.br>
2026-07-04 20:42:25 -0300

    feat(useLuva): cria a classe de conaxao com a luva

--- src/api/Axios.ts
+++ src/api/Axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://orionapi-production.up.railway.app",
+  baseURL: "http://localhost:8000",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
